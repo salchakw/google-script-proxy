abb521230b8a4bf6a5967bbce44812de1cd8821f
salchakw <140530227+salchakw@users.noreply.github.com>
2025-04-09 09:10:41 +0000

    Update booking.js add options

--- api/booking.js
+++ api/booking.js
@@ -1,6 +1,14 @@
 export default async function handler(req, res) {
   const targetUrl = req.query.url;
 
+  // Ответ на OPTIONS-запрос (Preflight запрос)
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
+
   if (!targetUrl) {
     return res.status(400).json({ success: false, message: 'Missing Google Script URL' });
   }
@@ -28,8 +36,4 @@ export default async function handler(req, res) {
     console.error("Ошибка при запросе к Google Apps Script:", error); // Логирование ошибки
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    
-    return res.status(500).json({ success: false, message: error.message });
-  }
-}
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type
